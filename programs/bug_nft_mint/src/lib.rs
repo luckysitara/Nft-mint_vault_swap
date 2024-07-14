a718596b9179620060a130450bc2976f1d540fa5
luckysitara <udoessienprecious@gmail.com>
2024-07-14 11:22:16 +0000

    writing libs.rs

--- programs/bug_nft_mint/src/lib.rs
+++ programs/bug_nft_mint/src/lib.rs
@@ -1,35 +1,42 @@
 use anchor_lang::prelude::*;
 
+// Modules for organizing code
 pub mod constants;
 pub mod error;
 pub mod instructions;
 pub mod state;
 
+// Re-export modules for external use
 pub use constants::*;
 pub use instructions::*;
 pub use state::*;
 
+// Define the program ID
 declare_id!("7B7qYnQAV8SfuxsnkXeWm63H8vYwuKxmWX55avBARUCL");
 
+// Anchor ID implementation for Core
 #[derive(Clone)]
 pub struct Core;
 
 impl anchor_lang::Id for Core {
     fn id() -> Pubkey {
-        mpl_core::ID
+        mpl_core::ID // Replace with actual ID if necessary
     }
 }
 
+// Anchor program module
 #[program]
 pub mod tims_nft_mint {
     use super::*;
 
-    /// Init protocol config and accounts
+    /// Initializes the protocol configuration and accounts.
+    /// This function should be called once to set up the program.
     pub fn init(ctx: Context<InitContext>) -> Result<()> {
         InitContext::init(ctx)
     }
 
-     /// Create a MPL Core collection 
+    /// Creates a MPL Core collection.
+    /// This function allows creating a new collection within the protocol.
     pub fn create_collection(
         ctx: Context<CreateCollectionContext>,
         params: CreateCollectionParams,
@@ -37,19 +44,22 @@ pub mod tims_nft_mint {
         CreateCollectionContext::create_collection(ctx, params)
     }
 
-     /// Create a MPL Core asset from a collection
+    /// Mints a MPL Core asset from a collection.
+    /// This function allows creating new assets within an existing collection.
     pub fn mint_asset(ctx: Context<MintFromCollectionContext>, params: MintFromColParams) -> Result<()> {
         MintFromCollectionContext::mint_from_collection(ctx, params)
     }
 
-     /// Lock asset in vault
-     pub fn lock_in_vault(ctx: Context<LockAssetInVaultContext>) -> Result<()> {
+    /// Locks an asset in a vault.
+    /// This function locks an existing asset within a designated vault.
+    pub fn lock_in_vault(ctx: Context<LockAssetInVaultContext>) -> Result<()> {
         LockAssetInVaultContext::lock_asset_in_vault(ctx)
     }
-    /// Purchase an asset 
+
+    /// Purchases an asset.
+    /// This function enables purchasing an asset within the protocol.
     pub fn purchase(ctx: Context<PurchaseContext>) -> Result<()> {
         PurchaseContext::purchase_asset(ctx)
     }
-
 }
 
